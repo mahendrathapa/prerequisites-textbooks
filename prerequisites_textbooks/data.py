34d755441f9939a46ec7a5c27f2d4a2f00db2c39
Mahendra Thapa <mahendra@fusemachines.com>
2020-11-12 17:37:37 +0545

    Generate pre-requisite relations from concepts
    To generate pre-requisite relations from given wikipedia concepts using
    metropolis-hasting algorithm.

---
+++ prerequisites_textbooks/data.py
@@ -0,0 +1,14 @@
+wikipedia_concepts = [
+    'concept_1',
+    'concept_2',
+    'concept_3'
+]
+
+sub_chapters = [
+    'sub_chapter_1',
+    'sub_chapter_2',
+    'sub_chapter_3'
+]
+
+total_wikipedia_concepts = len(wikipedia_concepts)
+total_sub_chapters = len(sub_chapters)
